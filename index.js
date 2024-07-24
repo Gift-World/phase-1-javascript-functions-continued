a66b211818a25b5ee0e802fa3e342c9d6a526797
charles Gift <charlesgiftangila@gmail.com>
2024-07-24 13:44:01 +0300

    *

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function saturdayFun(activity="roller-skate") {
 function mondayWork(activity="go to the office") {
     return(`This Monday, I will ${activity}.`)
 }
-function wrapAdjective(emphatic="special") {
+function wrapAdjective(emphatic="*") {
     return function(data="a hard worker"){
         return(`You are ${emphatic}${data}${emphatic}!`)
     }
